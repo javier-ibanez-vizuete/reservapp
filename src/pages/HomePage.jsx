92c4d011f376627a4df5a19445589f63f074c072
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2025-09-04 19:45:35 +0200

    doc(app): add initial db to the project

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,11 +2,11 @@ import { useContext } from "react";
 import { LanguageContext } from "../contexts/LanguageContext";
 
 export const HomePage = () => {
-	const { getText } = useContext(LanguageContext);
+    const { getText } = useContext(LanguageContext);
 
-	return (
-		<section>
-			<h1>{getText("h1HomePage")}</h1>
-		</section>
-	);
+    return (
+        <section>
+            <h1>{getText("h1HomePage")}</h1>
+        </section>
+    );
 };
